c9893d468210fb02bc4a884e88d8c3d9bbb9e409
Yemi Fakorede <yemifakorede@gmail.com>
2020-06-26 14:42:16 -0400

    completed knowledge panel

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,30 @@ const nameDiv = document.getElementById('name-div')
 const knowledgeAtomicNumber = document.getElementById('knowledge-atomic-number')
 const knowledgeSymbol = document.getElementById('knowledge-symbol')
 const knowledgeName = document.getElementById('knowledge-name')
+const knowledgeGroupBlock = document.getElementById('knowledge-group-block')
+const knowledgeStandardState = document.getElementById('knowledge-standard-state')
+const knowledgeAtomicMass = document.getElementById('knowledge-atomic-mass')
+const knowledgeAtomicRadius = document.getElementById('knowledge-atomic-radius')
+const knowledgeDensity = document.getElementById('knowledge-density')
+const knowledgeYearDiscovered = document.getElementById('knowledge-year-discovered')
+const knowledgeWiki = document.getElementById('knowledge-wiki')
 
+function createWikiUrl(string) {
+    return "https://www.wikipedia.org/wiki/" + string
+}
 
 function updateKnowledgePanel(e) {
     knowledgeAtomicNumber.innerHTML = e.atomicNumber
     knowledgeSymbol.innerHTML = e.symbol
     knowledgeName.innerHTML = e.name
+    knowledgeAtomicMass.innerHTML = e.atomicMass
+    knowledgeGroupBlock.innerHTML = e.groupBlock
+    knowledgeStandardState.innerHTML = e.standardState
+    knowledgeAtomicRadius.innerHTML = e.vanDelWaalsRadius
+    knowledgeDensity.innerHTML = e.density
+    knowledgeYearDiscovered.innerHTML = e.yearDiscovered
+    // knowledgeWiki.innerHTML = "<a href='https://www.wikipedia.org/wiki/" + e.name + "'" + e.name + "</a>"
+    knowledgeWiki.innerHTML = "<a href='" + createWikiUrl(e.name) + "' target='_blank'>" + e.name + " <i class='fas fa-external-link-alt'></a>"
 }
 
 
